3110ecd4e0237c9c07a386e1349b9650b517f519
Sébastien Denis <sebastien.denis.sin@gmail.com>
2022-07-20 19:04:38 +0200

    fix(android): fix app/build.gradle with implemetation AppComapt

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 import styles from "./styles";
 
-export const CTA = ({ label, onPress }) => {
+export const CTA = ({ label, onPress, testID }) => {
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-      <Text style={styles.text}>{label}</Text>
+      <Text testID={testID} style={styles.text}>
+        {label}
+      </Text>
     </TouchableOpacity>
   );
 };
